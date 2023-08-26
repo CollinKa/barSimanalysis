dfb6e63c49e8a2d1a68c0a36ee09295333425d25
Haoliang Zheng zheng.1947@buckeyemail.osu.edu <zheng@cms3.physics.ucsb.edu>
2023-08-25 17:06:06 -0700

    one hit per layer

--- cutCheck.cpp
+++ cutCheck.cpp
@@ -35,6 +35,37 @@ public:
         return hitN;
     }
 
+    int ex1HitPLay(mqROOTEvent* myROOTEvent) {
+        int numScintHits=myROOTEvent->GetScintRHits()->size();
+        std::vector<int> layerListV;
+        int hitN;
+        int layerN;
+
+        for (int h =0; h < numScintHits; h++)
+        {
+            hitN = myROOTEvent->GetScintRHits()->at(h)->GetCopyNo();
+
+            //exclude the veto pannals
+            if (hitN <= 67 || hitN >= 83){
+                //convert scitillator number into layer number
+                layerN = hitN/216;
+                layerListV.push_back(layerN);
+            }
+                
+
+        }
+
+        // Convert the vector to a set
+        std::set<int> layerListS(layerListV.begin(), layerListV.end());
+        int layS = layerListS.size(); 
+        int layV = layerListV.size();
+
+        if (layS == 4 && layV==4){return 1;}
+        else {return 0;}
+            
+
+    }
+
 };
 
 
@@ -54,11 +85,16 @@ void cutCheck()
         //std::cout << numScintHits << std::endl;
         if (numScintHits > 0) {
             CutTools cut1;
+            /*
             int result = cut1.AL1HitPLay(myROOTEvent);
             if (result > 0){
                 
                 std::cout <<"result" <<result << std::endl;
             }
+            */
+            int result = cut1.ex1HitPLay(myROOTEvent);
+            std::cout << result << std::endl;
+            
             
         }
     }
