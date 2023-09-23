90515597f1e9dd8278c4f0c08ea71c638f4b7c25
Haoliang Zheng zheng.1947@buckeyemail.osu.edu <zheng@cms3.physics.ucsb.edu>
2023-09-23 09:38:11 -0700

    find the event number of interest event

---
+++ summingHist_withPhoton.py
@@ -0,0 +1,38 @@
+import os
+
+event_sums = {}
+
+input_files = []
+
+directory= "/net/cms26/cms26r0/zheng/barSimulation/withPhotonAnalysis/resultsWithPhoton/"
+base_name = "hist"
+
+for filename in os.listdir(directory):
+    if filename.startswith(base_name) and filename.endswith(".txt"):
+        input_files.append(directory+filename)
+
+for filepath in input_files:
+    with open(filepath, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            if ':' in line:
+                parts = line.split(':')
+
+                event = parts[0]
+                count = parts[1]
+                print(event)
+                print(count)
+                print(filename)
+                #count = int(count.strip())
+
+                # Update the count in the dictionary
+                #if event in event_sums:
+                #    event_sums[event] += count
+                #else:
+                #    event_sums[event] = count
+            #else:
+            #    event = line
+            #    count = []
+
+#for event, count in event_sums.items():
+    #print(f"{event}: {count}")
