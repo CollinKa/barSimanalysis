f9fd39ee5c035ed5ee9d6a544c5840af050467e9
CollinKa <colin.ka9@gmail.com>
2023-12-18 23:12:24 -0500

    update

--- cutCheck.cpp
+++ cutCheck.cpp
@@ -332,58 +332,59 @@ public:
     }
 
     void MakeChanHistogram(mqROOTEvent* myROOTEvent, TH1F* SumEDistribution0, TH1F* SumEDistribution1, TH1F* SumEDistribution2, TH1F* SumEDistribution3,TH1F* SumEDistribution4,TH1F* SumEDistribution5){
-        int result = AL1HitPLay(myROOTEvent);
+        //int result = AL1HitPLay(myROOTEvent);
 
 
 
-        if (result == 1)
+        //if (result == 1)
+        //{
+        int numScintHits=myROOTEvent->GetScintRHits()->size(); //number of scitillator get hit in a event
+        std::set<int> layer;
+
+        std::map<int, double> mapOfEnergy;//it provide the summing deposited 
+        const int numberOfChannel = 64;
+        const double defaultE = 0.0;
+        for (int i = 0; i < numberOfChannel; ++i) {mapOfEnergy[i] = defaultE;}
+        int hitN;
+        int layerN;
+        for (int h =0; h < numScintHits; h++)
         {
-            int numScintHits=myROOTEvent->GetScintRHits()->size(); //number of scitillator get hit in a event
-            std::set<int> layer;
+            hitN = myROOTEvent->GetScintRHits()->at(h)->GetCopyNo();
+            double energy = myROOTEvent->GetScintRHits()->at(h)->GetEDep();
 
-            std::map<int, double> mapOfEnergy;//it provide the summing deposited 
-            const int numberOfChannel = 64;
-            const double defaultE = 0.0;
-            for (int i = 0; i < numberOfChannel; ++i) {mapOfEnergy[i] = defaultE;}
-            int hitN;
-            int layerN;
-            for (int h =0; h < numScintHits; h++)
-            {
-                hitN = myROOTEvent->GetScintRHits()->at(h)->GetCopyNo();
-                double energy = myROOTEvent->GetScintRHits()->at(h)->GetEDep();
-
-                //exclude the veto pannals
-                if (hitN <= 64)
-                {   
-                    int layerN = (hitN-1)/16; //old mapping
-                    mapOfEnergy[layerN] += energy;
-
-                }
+            //exclude the veto pannals
+            if (hitN <= 64)
+            {   
+                int layerN = (hitN-1)/16; //old mapping
+                mapOfEnergy[layerN] += energy;
 
             }
 
-            double Esum4layer = 0.0;
-
-            for (const auto& pair : mapOfEnergy)
-            {
-                int layer = pair.first; 
-                double Etot = pair.second; //total deposit energy on a bar
-                //if (Etot > 0.0)
-                //{
-                    if (layer == 0){SumEDistribution0->Fill(Etot);}
-                    if (layer == 1){SumEDistribution1->Fill(Etot);}
-                    if (layer == 2){SumEDistribution2->Fill(Etot);}
-                    if (layer == 3){SumEDistribution3->Fill(Etot);}
-                    Esum4layer += Etot;
-
-                //}
-                
-            }
-            SumEDistribution4->Fill(Esum4layer);
         }
+
+        double Esum4layer = 0.0;
+
+        for (const auto& pair : mapOfEnergy)
+        {
+            int layer = pair.first; 
+            double Etot = pair.second; //total deposit energy on a bar
+            //if (Etot > 0.0)
+            //{
+                if (layer == 0){SumEDistribution0->Fill(Etot);}
+                if (layer == 1){SumEDistribution1->Fill(Etot);}
+                if (layer == 2){SumEDistribution2->Fill(Etot);}
+                if (layer == 3){SumEDistribution3->Fill(Etot);}
+                Esum4layer += Etot;
+
+            //}
+            
+        }
+        SumEDistribution4->Fill(Esum4layer);
+        //}
 
 
         //plot for all of kinds of event
+        /*
         int numScintHits=myROOTEvent->GetScintRHits()->size(); //number of scitillator get hit in a event
         int hitN;
         double Sumenergy = 0.0;
@@ -396,6 +397,8 @@ public:
             if (hitN <= 64){Sumenergy += energy;} 
         }
         SumEDistribution5->Fill(Sumenergy);
+        */
+        
 
 
 
@@ -437,7 +440,7 @@ void cutCheck()
     TH1F* SumEDistribution2 = new TH1F("E distribution2", "E(sum along layer 2 for 1 hit per layer event) distribution", 120, -600, 600); //energy can reach up to 600MeV, but 0-100MeV is sufficient for see the trend
     TH1F* SumEDistribution3 = new TH1F("E distribution3", "E(sum along layer 3 for 1 hit per layer event) distribution", 120, -600, 600); //energy can reach up to 600MeV, but 0-100MeV is sufficient for see the trend
     TH1F* SumEDistribution4 = new TH1F("E distribution4 layer", "E(sum along 4 layers for 1 hit per layer event) distribution", 120, -600, 600);
-    TH1F* SumEDistribution5 = new TH1F("E distribution4 layers (not AL1hitpL)", "E(sum along 4 layers ) distribution", 120, -600, 600);
+    TH1F* SumEDistribution5 = new TH1F("E distribution4 layers (not AL1hitpL)", "E(sum along 4 layers ) distribution", 600, -600, 600);
     
     TChain ch("Events");
     ch.Add(fileName);
