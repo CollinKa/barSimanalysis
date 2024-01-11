d72882937df4f237770691f5bc3c1c8cda9a361c
Haoliang Zheng zheng.1947@buckeyemail.osu.edu <zheng@cms3.physics.ucsb.edu>
2024-01-11 13:34:38 -0800

    add new file for flat tree processing

---
+++ FlatCutCheck.py
@@ -0,0 +1,14 @@
+"""
+1-11 24
+the goal of this file is to check the following things after the fix deposited energy when facing 
+compton scater and muon capture process.
+
+1. first two geometric cuts, npe and time cut while using with and without photon data.
+
+"""
+
+import ROOT
+import os
+import math
+import random
+import numpy as np
