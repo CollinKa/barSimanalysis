da0645b6cef2769f0c2d9ef87c2e308eb1a07a4e
CollinKa <Colin.ka9@gmail.com>
2024-01-16 08:30:40 -0800

    chage the way of counting hit for different method and add the method for NPE and time cut

--- FlatMethod.py
+++ FlatMethod.py
@@ -1,90 +1,98 @@
+"""
+
+
+"""
+
+
+
+
+
 import math
 import random
 
 
-#Machine epsilon for float: 2.220446049250313e-16
-
-FloatEq = 2.220446049250313e-16
-
-# this method can only being used for without photon data. beacaseu with photon data doesn't need to use P cut!
-def geometricCut(layers, chan, nPE):
-
+# this is used for without photon data
+def geometricCut_noP(layers, chan, nPE):
+    NPE_Cut = 0.6 # based on previous analysis, NPE >= 0.6 can reach the performance like P cut.
     events_AL_4_layer_got_hits = False
     events_with_4_unique_hits = False
-    events_AL_4_layer_got_hits_p = False
-    events_with_4_unique_hits_p = False
-    cosPanel_hit = False
-    beamPanel = False
-    cosPanel_hit_p = False
-    beamPanel_p = False
+    cosPanel_hit = True
+    beamPanel_hit = True
+    beamPanelNPE = 0   
+    hits = 0 
 
 
     unique_layers = set()
-    barlayerlist = []
+
+    for i in range(len(layers)):
+        if (chan[i] <= 65) and (nPE[i] >= NPE_Cut):
+            unique_layers.add(layers[i])
+            hits = hits + 1
+        
+        if (chan[i] == 71 or chan[i] == 75)and (nPE[i] >= NPE_Cut):
+            beamPanelNPE += nPE[i]
+
+        
+        if (chan[i] >= 68 and chan[i] != 71 and chan[i] != 75)and (nPE[i] >= NPE_Cut):
+            cosPanel_hit = False
+
+        
+    if len(unique_layers) == 4:
+        events_AL_4_layer_got_hits = True
+
+    if len(unique_layers) == 4 and hits == 4:
+        events_with_4_unique_hits = True
 
 
-    hits=0
+    #if the sum of deposited energy on beam panel reach 1MeV, then we claim it fail the beamVeto cut.
+    #The nPE/MeV ratio for beam panels is 75.1
+    if beamPanelNPE >= 75.1:
+        beamPanel_hit = False
+    
+    return events_AL_4_layer_got_hits,events_with_4_unique_hits,cosPanel_hit,beamPanel_hit
+
+# this method can only being used for without photon data. beacaseu with photon data doesn't need to use P cut!
+def geometricCut_P(layers, chan, nPE):
+    
+    events_AL_4_layer_got_hits_p = False
+    events_with_4_unique_hits_p = False
+    cosPanel_hit_p = True
+    beamPanel_p = True
 
     unique_layers_p = set()
-    barlayerlist_p = []
     hits_p=0
+    beamPanelNPE_p = 0
 
 
     for i in range(len(layers)):
         detect = 1-math.exp(-1*nPE[i])
         detector = random.random()
 
-        if chan[i] <= 65 and abs(nPE[i]) > FloatEq:
-            unique_layers.add(layers[i])
-            barlayerlist.append(layers[i])
-            hits = hits + 1
-        
-        
-
         if chan[i] <= 65 and (detector < detect):
             unique_layers_p.add(layers[i])
-            barlayerlist_p.append(layers[i])
             hits_p = hits_p + 1
         
-        if (chan[i] == 71 or chan[i] == 75)and abs(nPE[i]) > FloatEq:
-            beamPanel = True
 
         if (chan[i] == 71 or chan[i] == 75)and (detector < detect):
-            beamPanel_p = True
+            beamPanelNPE_p += nPE[i]
 
-        if (chan[i] >= 68 and chan[i] != 71 and chan[i] != 75)and abs(nPE[i]) > FloatEq:
-            cosPanel_hit = True
-        
         if (chan[i] >= 68 and chan[i] != 71 and chan[i] != 75)and (detector < detect):
-            cosPanel_hit_p = True
-
-    
-    if len(unique_layers) == 4:
-        events_AL_4_layer_got_hits = True
-
-    if len(unique_layers) == 4 and hits == 4:
-        events_with_4_unique_hits = True
-    
-    cosPanel_hit = cosPanel_hit and events_with_4_unique_hits
-
-    beamPanel = beamPanel and cosPanel_hit
+            cosPanel_hit_p = False
 
 
     if len(unique_layers_p) == 4:
         events_AL_4_layer_got_hits_p = True
 
-    if len(barlayerlist_p) == 4 and hits == 4:
+    if len(unique_layers_p) == 4 and hits == 4:
         events_with_4_unique_hits_p = True
 
-    cosPanel_hit_p = cosPanel_hit_p and events_with_4_unique_hits_p
-
-    beamPanel_p = beamPanel_p and cosPanel_hit_p
-
+    if beamPanelNPE_p >= 75.1:
+        beamPanel_p = False
     
     
 
     
-    return events_AL_4_layer_got_hits,events_with_4_unique_hits,events_AL_4_layer_got_hits_p,events_with_4_unique_hits_p
+    return events_AL_4_layer_got_hits_p,events_with_4_unique_hits_p,cosPanel_hit_p,beamPanel_p
 
 
 # this one doesn't come with probability cut. It can work with any kind of data
@@ -94,12 +102,11 @@ def geometricCut_WithPhoton(layers, chan, nPE):
     events_with_4_unique_hits = False
     
     cosPanel_hit = False
-    beamPanel = False
-
+    beamPanel_hit = False
+    beamPanelNPE = 0
 
 
     unique_layers = set()
-    barlayerlist = []
 
 
     hits=0
@@ -109,16 +116,15 @@ def geometricCut_WithPhoton(layers, chan, nPE):
     for i in range(len(layers)):
 
 
-        if chan[i] <= 65 and (nPE[i] > -FloatEq):
+        if chan[i] <= 65 and (nPE[i] >= 1):
             unique_layers.add(layers[i])
-            barlayerlist.append(layers[i])
             hits = hits + 1
         
-        if (chan[i] == 71 or chan[i] == 75 ) and (nPE[i] > -FloatEq):
-            beamPanel = True
+        if (chan[i] == 71 or chan[i] == 75 ) and (nPE[i] >= 1):
+            beamPanelNPE += 1
 
-        if (chan[i] >= 68 and chan[i] != 71 and chan[i] != 75) and (nPE[i] > -FloatEq):
-            cosPanel_hit = True
+        if (chan[i] >= 68 and chan[i] != 71 and chan[i] != 75) and (nPE[i] >= 1):
+            cosPanel_hit = False
         
 
     
@@ -128,17 +134,47 @@ def geometricCut_WithPhoton(layers, chan, nPE):
     if len(unique_layers) == 4 and hits == 4:
         events_with_4_unique_hits = True
     
-    cosPanel_hit = cosPanel_hit and events_with_4_unique_hits
-
-    beamPanel = beamPanel and cosPanel_hit
+    if beamPanelNPE >= 75:
+        beamPanel_hit = False
+        
 
     
-    return events_AL_4_layer_got_hits,events_with_4_unique_hits
+    return events_AL_4_layer_got_hits,events_with_4_unique_hits, cosPanel_hit, beamPanel_hit
 
 
-# panel,NPE,time 
-def panelCut_withPhoton(chan,nPE):
-    event_cosVeto = False
-    event_beamVeto = False
+#NPE,time 
+def NPE_TimeCut_withPhoton(chan,layer,nPE,time):
+   NPECut = True
+   TimeCut = False
+   nPEList = list()   
+   Lay0time = list()
+   Lay3time = list()
+   nPEList = []
 
-    for i in range(len(nPE)):  
+   for i in range(len(layer)):
+       if chan[i] <= 65 and (nPE[i] >= 1):
+           nPEList.append(nPE[i])
+           if layer[i] == 0:
+               Lay0time.append(time[i])
+           if layer[i] == 3:  
+               Lay3time.append(time[i])
+
+   if len(nPEList) == 0:
+       return False, False
+
+   maxNPE = max(nPEList)
+   minNPE = min(nPEList)
+   
+
+   if maxNPE/minNPE > 10:
+       NPECut = False      
+
+   if len(Lay3time) == 0 or len(Lay0time) == 0:
+       return NPECut,False
+
+   if (max(Lay3time)-min(Lay0time) < 15.04) or (max(Lay0time)-min(Lay3time) < 15.04):
+       TimeCut = True    
+
+   return NPECut,TimeCut
+
+
