e3ad5bd33dfe71c1426cf0dc6507e15b8df58dd9
Collin Zheng <61294685+CollinKa@users.noreply.github.com>
2024-01-23 01:02:30 -0500

    add the source/spe peak correction for each channel

--- flatlight.py
+++ flatlight.py
@@ -47,6 +47,9 @@ def simToDataScint(simChannel):
     else:
         print("Error: simChannel out of range")
         return -1
+#
+cali =[9.7,8.4,5.0,3.6,8.2,8.2,6.8,4.6,8.7,7.1,6.4,7.5,8.7,11.8,9.7,8.1,8.6,9.1,6.4,7.5,9.8,8.8,11,6.6,8.6,11,7.3,8.3,2.6,6.2,7.0,7.2,8.7,7.7,6.8,7.8,8.8,8.5,3.5,8.2,7.6,6.8,9.5,8.5,6.3,6.7,7.6,7.9,9.0,8.3,8.3,6.9,6.0,8.0,5.5,5.5,6.6,8.1,8.4,8.7,9.2,7.6,5.9,8.2] 
+    
 
 # Function to populate the vectors in the flattened tree for ScintHits
 def populate_vectors_scint(input_tree, scint_copyNo, scint_layer, scint_nPE, scint_time):
@@ -69,7 +72,9 @@ def populate_vectors_scint(input_tree, scint_copyNo, scint_layer, scint_nPE, sci
         # add the hit to the temporary array, using the energy deposition as the nPE and converting it using a measure scale factor
         # also multiply by 7.5/11, the mean ratio of the measured nPE to the simulated nPE for the same geometry
         # this way we have three different ways to scale the nPE according to the geometry and correcting for the difference between simulation and data
-        simToDataScale = 0.682 #7.5/11
+        #simToDataScale = 0.682 #7.5/11
+        transferChan = simToDataScint(tempCopyNo)
+        simToDataScale = cali[transferChan]/11
         if(tempCopyNo == 67 or tempCopyNo == 68):
             nPEPerMeV = 110.2*simToDataScale #measured using bar cosmic dataset
         elif(tempCopyNo == 73 or tempCopyNo == 74 or tempCopyNo == 75 or tempCopyNo == 81 or tempCopyNo == 82 or tempCopyNo == 83):
